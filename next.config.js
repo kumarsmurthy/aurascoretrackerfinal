f907cffd23d2b1cd7b25fc230287a8dec1498ac9
dreaming <kumarsmurthy@gmail.com>
2024-10-02 18:33:26 -0500

    modified aura...tsx, and next.config.js files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,40 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'www.aurascore.app',
+          },
+        ],
+        destination: 'https://aurascore.app',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.aurascore.app',
+          },
+        ],
+        destination: 'https://aurascore.app/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
+
+// /** @type {import('next').NextConfig} */
+// #const nextConfig = {
+//  reactStrictMode: true,
+//  swcMinify: true,
+//}
+
+// module.exports = nextConfig
